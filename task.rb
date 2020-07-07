026c7fc2f711a91edb3336cd80fbfc1f45447da3
Yuta Suzuki <yutasuzuki@YMBP.local>
2020-07-07 22:22:33 +0900

    Ruby課題(応用)Q17.Q18提出致します

--- task.rb
+++ task.rb
@@ -154,7 +154,22 @@ def q16
 end
 
 class UserQ17
-  # 以下に回答を記載
+
+  def initialize ( ** params )
+   @name = params [ :name ]
+   @age = params [ :age ]
+   @gender = params [ :gender ]
+  end
+
+  def info
+    puts <<~TEXT
+    
+    名前 : #{ @name }
+    年齢 : #{ @age }
+    性別 : #{ @gender }
+    
+    TEXT
+  end
 
 end
 
@@ -170,7 +185,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize ( ** params )
+    @name = params [ :name ]
+    @age = params [ :age ]
+  end
 
+  def introduce
+    if @age > 20
+      "こんにちは〜、#{ @name }と申します。宜しくお願いいたします。"
+    else
+      "はいさいまいど〜、#{ @name }です！！！"
+    end
+  end
 end
 
 def q18
